fa7c233697d4bfa6ea91e4db8d79e118c442ae13
gabbydgab <gamba@gabbydgab.com>
2019-08-03 10:03:54 +0800

    initial structure

---
+++ tests/TestCase.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CodingMatters\ProductCatalogTest;
+
+/**
+ * Base test class
+ *
+ * @package CodingMatters\ProductCatalogTest
+ */
+class TestCase extends \Orchestra\Testbench\TestCase
+{
+}
